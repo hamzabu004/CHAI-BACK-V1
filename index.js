0b3d2c763596043caca0d0d4bcdd5332b99e7342
hamzabu004 <hamzabu2004@gmail.com>
2024-01-07 14:27:16 +0500

    update port var

--- index.js
+++ index.js
@@ -13,6 +13,6 @@ app.get('/admin', function(req, res){
     res.send('Helo Admin');
 });
 
-app.listen(process.env.PORT, function(){
-    console.log('Server is running on port ' + process.env.PORT);
+app.listen(process.env.V1_PORT, function(){    
+	console.log('Server is running on port ' + process.env.PORT);
 });
